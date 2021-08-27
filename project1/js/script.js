ea199f991860142c079915b6f7b0625a5bef4f35
Volodymyr <pustovetov73@gmail.com>
2021-08-27 17:27:46 +0300

    Start

---
+++ project1/js/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm('Are you hero');
+// console.log(result);
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer);
+
+const answer = [];
+
+// answer[0] = prompt('Как Ваше имя?', '');
+// answer[1] = prompt('Как имя?', '');
+// answer[2] = prompt('Как Ваше?', '');
+
+console.log('arr');
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !== '6');
+
+const isChecked = false,
+      isClose = false;
+      
+      
+console.log(isChecked || !isClose);
