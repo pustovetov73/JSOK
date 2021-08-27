1db0888dfebc6236d73ba2d5dbe1254e0553e544
Vladimir <pustovetov73@gmail.com>
2021-08-27 20:56:09 +0300

    Первое практическое задание

--- project1/js/script.js
+++ project1/js/script.js
@@ -1,35 +1,41 @@
 "use strict";
 
-// alert('Hello');
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+Проверить, чтобы все работало без ошибок в консоли */
 
-// const result = confirm('Are you hero');
-// console.log(result);
-// const answer = prompt('Вам есть 18?', '18');
-// console.log(answer);
 
-const answer = [];
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// answer[0] = prompt('Как Ваше имя?', '');
-// answer[1] = prompt('Как имя?', '');
-// answer[2] = prompt('Как Ваше?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log('arr');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let incr = 10,
-    decr = 10;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-incr++;
-decr--;
-
-console.log(incr);
-console.log(decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-const isChecked = false,
-      isClose = false;
-      
-      
-console.log(isChecked || !isClose);
+console.log(personalMovieDB);
