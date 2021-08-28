d693fb57347031b4f74890443bb1c6828ec702f8
Volodymyr <pustovetov73@gmail.com>
2021-08-28 14:24:23 +0300

    2 проктическое занятие пока нечего не понятно

--- project1/js/script.js
+++ project1/js/script.js
@@ -30,21 +30,38 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }      
+
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Вы любитель');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('У Вас достойный уровень');
+
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы профи в киномотагрофии');
+} else {
+    console.log('eroor');
+}
 
 console.log(personalMovieDB);
  
-if (4 == 4) {
-    console.log('OK!');
-} else {
-    console.log('Error');
-}
+
+
+
+
 
                             // УСЛОВИЯ
 
