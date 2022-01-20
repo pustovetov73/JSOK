21222572d760d7000f2ff73bd4cb12b3cb8d51cf
VladimirWork <pustovetov73@gmail.com>
2022-01-20 16:34:08 +0200

    Массив и псевдомасив

--- project1/js/script.js
+++ project1/js/script.js
@@ -279,3 +279,31 @@ console.log(border);
                 console.log(value);
             }
             
+            // МАСИИВЫ и псевдомасивы
+
+
+
+            
+const arr = [2, 3, 6, 8, 10];
+
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+
+arr.forEach(function(item, i, ) {
+
+});
+
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+for (let value of arr) {
+    console.log(value);
+}
